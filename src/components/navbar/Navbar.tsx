eeddd38f99f27b69a8c144ffd263ef05699520b0
James Robinson II <jrob286@gmail.com>
2023-02-17 13:39:35 -0500

    goody

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,13 +2,27 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { withTheme } from '@emotion/react';
 
-import { Grid, AppBar as MuiAppBar, Toolbar } from '@mui/material';
+import {
+  Grid,
+  AppBar as MuiAppBar,
+  Toolbar,
+  IconButton as MuiIconButton,
+} from '@mui/material';
+
+import { Menu as MenuIcon } from '@mui/icons-material';
 
 import NavbarUserDropdown from './NavbarUserDropdown';
 
 const AppBar = styled(MuiAppBar)`
   background: ${(props) => props.theme.header.background};
   color: ${(props) => props.theme.header.color};
+`;
+
+const IconButton = styled(MuiIconButton)`
+  svg {
+    width: 22px;
+    height: 22px;
+  }
 `;
 
 type NavbarProps = {
@@ -20,7 +34,18 @@ const Navbar: React.FC<NavbarProps> = ({ onDrawerToggle }) => {
     <>
       <AppBar position="sticky" elevation={0}>
         <Toolbar>
-          <Grid container alignItems="center" justifyContent="flex-end">
+          <Grid container alignItems="center">
+            <Grid item sx={{ display: { xs: 'block', md: 'none' } }}>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={onDrawerToggle}
+                size="large"
+              >
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+            <Grid item xs />
             <Grid item>
               <NavbarUserDropdown />
             </Grid>
